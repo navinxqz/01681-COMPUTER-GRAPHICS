544cc268f8ab682af635c446c82b0165dc981440
Navin <navinmdnawshin@gmail.com>
2025-03-15 18:49:54 +0600

    win screen size fix

--- labtask1(a)/CountryFlag/CountryFlag/Source.cpp
+++ labtask1(a)/CountryFlag/CountryFlag/Source.cpp
@@ -1,7 +1,6 @@
 #include <GL/glut.h>
 #include <math.h>
 
-//rectangle func
 void drawRectangle(float x1, float y1, float x2, float y2, float r, float g, float b) {
     glColor3f(r, g, b);
     glBegin(GL_QUADS);
@@ -11,31 +10,31 @@ void drawRectangle(float x1, float y1, float x2, float y2, float r, float g, flo
     glVertex2f(x1, y2);
     glEnd();
 }
-void drawEngland(float x1, float y1, float x2, float y2) {
+void England(float x1, float y1, float x2, float y2) {
     drawRectangle(x1, y1, x2, y2, 1.0f, 1.0f, 1.0f); // White bg
     drawRectangle((x1 + x2) / 2 - 0.03f, y1, (x1 + x2) / 2 + 0.03f, y2, 1.0f, 0.0f, 0.0f); // Vertical
     drawRectangle(x1, (y1 + y2) / 2 - 0.03f, x2, (y1 + y2) / 2 + 0.03f, 1.0f, 0.0f, 0.0f); // Horizontal
 }
 
-void drawFinland(float x1, float y1, float x2, float y2) {
+void Finland(float x1, float y1, float x2, float y2) {
     drawRectangle(x1, y1, x2, y2, 1.0f, 1.0f, 1.0f);
     drawRectangle(x1 + 0.07f, y1, x1 + 0.14f, y2, 0.0f, 0.0f, 1.0f); // Vertical
     drawRectangle(x1, (y1 + y2) / 2 - 0.03f, x2, (y1 + y2) / 2 + 0.03f, 0.0f, 0.0f, 1.0f); // Horizontal
 }
 
-void drawItaly(float x1, float y1, float x2, float y2) {
+void Italy(float x1, float y1, float x2, float y2) {
     drawRectangle(x1, y1, x1 + (x2 - x1) / 3, y2, 0.0f, 0.5f, 0.0f); // Green
     drawRectangle(x1 + (x2 - x1) / 3, y1, x1 + 2 * (x2 - x1) / 3, y2, 1.0f, 1.0f, 1.0f); // White
     drawRectangle(x1 + 2 * (x2 - x1) / 3, y1, x2, y2, 1.0f, 0.0f, 0.0f); // Red
 }
 
-void drawFrance(float x1, float y1, float x2, float y2) {
+void France(float x1, float y1, float x2, float y2) {
     drawRectangle(x1, y1, x1 + (x2 - x1) / 3, y2, 0.0f, 0.0f, 1.0f); // Blue
     drawRectangle(x1 + (x2 - x1) / 3, y1, x1 + 2 * (x2 - x1) / 3, y2, 1.0f, 1.0f, 1.0f); // White
     drawRectangle(x1 + 2 * (x2 - x1) / 3, y1, x2, y2, 1.0f, 0.0f, 0.0f); // Red
 }
 
-void drawGermany(float x1, float y1, float x2, float y2) {
+void Germany(float x1, float y1, float x2, float y2) {
     drawRectangle(x1, y1, x2, y1 + (y2 - y1) / 3, 0.0f, 0.0f, 0.0f); // Black
     drawRectangle(x1, y1 + (y2 - y1) / 3, x2, y1 + 2 * (y2 - y1) / 3, 1.0f, 0.0f, 0.0f); // Red
     drawRectangle(x1, y1 + 2 * (y2 - y1) / 3, x2, y2, 1.0f, 1.0f, 0.0f); // Yellow
@@ -47,11 +46,11 @@ void display() {
     float height = 0.45f;
     float spacing = 0.02f;
 
-    drawEngland(-1.0f, -height / 2, -1.0f + width - spacing, height / 2);
-    drawFinland(-1.0f + width + spacing, -height / 2, -1.0f + 2 * width - spacing, height / 2);
-    drawItaly(-1.0f + 2 * width + spacing, -height / 2, -1.0f + 3 * width - spacing, height / 2);
-    drawFrance(-1.0f + 3 * width + spacing, -height / 2, -1.0f + 4 * width - spacing, height / 2);
-    drawGermany(-1.0f + 4 * width + spacing, -height / 2, 1.0f, height / 2);
+    England(-1.0f, -height / 2, -1.0f + width - spacing, height / 2);
+    Finland(-1.0f + width + spacing, -height / 2, -1.0f + 2 * width - spacing, height / 2);
+    Italy(-1.0f + 2 * width + spacing, -height / 2, -1.0f + 3 * width - spacing, height / 2);
+    France(-1.0f + 3 * width + spacing, -height / 2, -1.0f + 4 * width - spacing, height / 2);
+    Germany(-1.0f + 4 * width + spacing, -height / 2, 1.0f, height / 2);
 
     glFlush();
 }
@@ -66,7 +65,7 @@ void init() {
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(1000, 500);
+    glutInitWindowSize(1000, 450);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("5 Flags");
     init();
