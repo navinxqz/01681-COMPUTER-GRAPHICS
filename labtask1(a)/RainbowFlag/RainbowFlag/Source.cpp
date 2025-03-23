04cc6d2de3e24bbebff686cc1c354967eecd361a
Navin <navinmdnawshin@gmail.com>
2025-03-23 12:59:38 +0600

    using vertex

--- labtask1(a)/RainbowFlag/RainbowFlag/Source.cpp
+++ labtask1(a)/RainbowFlag/RainbowFlag/Source.cpp
@@ -1,67 +1,48 @@
 #include <GL/glut.h>
 #include <iostream>
 #include <sstream>
+#include <iomanip>
 using namespace std;
 
-void conv(const char* hex, float& r, float& g, float& b){  //hex to RGB conv
-    int hr, hg, hb;
-    string hexStr(hex + 1); // Skip #
-    stringstream ss;
-    ss << std::hex << hexStr.substr(0, 2) << " "
-        << hexStr.substr(2, 2) << " "
-        << hexStr.substr(4, 2);
-    ss >> hr >> hg >> hb;
+void conv(const string& hex, float& r, float& g, float& b) {    //hex to rgb format
+    int hr = stoi(hex.substr(1, 2), nullptr, 16);
+    int hg = stoi(hex.substr(3, 2), nullptr, 16);
+    int hb = stoi(hex.substr(5, 2), nullptr, 16);
     r = hr / 255.0f;
     g = hg / 255.0f;
     b = hb / 255.0f;
 }
 
-void drawStripe(float yTop, float yBottom, const char* color, float flagLeft, float flagRight) {
+void drawStripe(const string& color, float y1, float y2) {
     float r, g, b;
     conv(color, r, g, b);
     glColor3f(r, g, b);
     glBegin(GL_QUADS);
-    glVertex2f(flagLeft, yTop);
-    glVertex2f(flagRight, yTop);
-    glVertex2f(flagRight, yBottom);
-    glVertex2f(flagLeft, yBottom);
+    glVertex2f(-0.8f, y1);
+    glVertex2f(0.8f, y1);
+    glVertex2f(0.8f, y2);
+    glVertex2f(-0.8f, y2);
     glEnd();
 }
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-    const char* colors[7] = {
-        "#9400D3",  // Violet
-        "#0000FF",  // Blue
-        "#00CED1",  // Cyan
-        "#008000",  // Green
-        "#FFA500",  // Orange
-        "#FFFF00",  // Yellow
-        "#FF0000"   // Red
-    };
-
-    float flagLeft = -0.6f;//boundary
-    float flagRight = 0.6f;
-    float flagTop = 0.5f;
-    float flagBottom = -0.5f;
-
-    float stripeHeight = (flagTop - flagBottom) / 7; //stripe height
-    float yTop = flagTop, yBottom = flagTop - stripeHeight;
-
-    for (int i = 0; i < 7; i++) {
-        drawStripe(yTop, yBottom, colors[i], flagLeft, flagRight);
-        yTop -= stripeHeight;
-        yBottom -= stripeHeight;
-    }
+    drawStripe("#9400D3", 0.7f, 0.5f); // purple
+    drawStripe("#0000FF", 0.5f, 0.3f); // Blue
+    drawStripe("#00CED1", 0.3f, 0.1f); // Cyan
+    drawStripe("#008000", 0.1f, -0.1f); // Green
+    drawStripe("#FFA500", -0.1f, -0.3f); // Orange
+    drawStripe("#FFFF00", -0.3f, -0.5f); // Yellow
+    drawStripe("#FF0000", -0.5f, -0.7f); // Red
     glutSwapBuffers();
 }
 
 void init() {
-    glClearColor(1.0, 1.0, 1.0, 1.0); //white bg
+    glClearColor(1.0, 1.0, 1.0, 1.0); // white bg
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-1, 1, -1, 1, -1, 1); //coordinate
+    glOrtho(-1, 1, -1, 1, -1, 1); // coordinates
 }
-
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
