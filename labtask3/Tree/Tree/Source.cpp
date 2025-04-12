b02e4fd2efe33f695b700eb557f7e1d36e6f4da4
Navin <navinmdnawshin@gmail.com>
2025-04-12 10:15:24 +0600

    changed tree

--- labtask3/Tree/Tree/Source.cpp
+++ labtask3/Tree/Tree/Source.cpp
@@ -9,7 +9,6 @@ void drawRectangle(float x1, float y1, float x2, float y2) {
     glVertex2f(x2, y1);
     glEnd();
 }
-
 void drawCircle(float cx, float cy, float radius, int segments = 36) {
     glBegin(GL_POLYGON);
     for (int i = 0; i < segments; i++) {
